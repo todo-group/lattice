024c36d76de18dfbc1d94a71cd65fe1689c21433
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-07-24 11:20:34 +0900

    Add kkr2cell tool

---
+++ tool/kkr2cell/kkr2cell.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sys
+import xml.etree.ElementTree as ET
+
+class unitcell:
+    """
+    Extract unitcell structure from AkaiKKR output
+
+    Attributes
+    ----------
+    dimension : int
+        dimension of space
+    bravais : string
+        type of Bravais lattice
+    lattice_constant : array of floats
+        lattice constans of Bravais lattice [a,b,c]
+    lattice_angle : array of floats
+        lattice angles of Bravais lattice [alpha,beta,gamma]
+    cell_bases : dimension x dimension matrix of floats
+        (column) basis vectors of unit cell
+    site_labels : list of strings
+        list of site labels
+    sites : list of [array of floats, int]
+        relative site coordinates and type of each site
+    coupling_constants = list of floats
+        list of coupling constants, J
+    bonds : list of [int, int, array of int, int]
+        source, target, cell offset of target, and type of each bond
+    """
+    def __init__(self, path = '', name = ''):
+        """
+        Paramters
+        ---------
+        path :
+           path to the input file
+        name :
+           name of unit cell type ('simple cubic' or 'square')
+        If both paramters are specified, 'path' precedes 'name'.
+        """
+        if (len(path) > 0):
+            self.parse(path)
+        else:
+            if (name == 'simple cubic'):
+                # simple cubic lattice
+                self.dimension = 3
+                self.bravais = 'sc'
+                self.lattice_constant = np.array([1.0, 1.0, 1.0])
+                self.lattice_angle = np.array([90.0, 90.0, 90.0])
+                self.cell_bases = np.eye(3, order='F')
+                self.site_labels = ['S',]
+                self.sites = [[np.array([0.0, 0.0, 0.0]), 0],]
+                self.coupling_constants = [1.0]
+                self.bonds = [[0, 0, np.array([1, 0, 0], np.int32), 0],
+                              [0, 0, np.array([0, 1, 0], np.int32), 0],
+                              [0, 0, np.array([0, 0, 1], np.int32), 0]]
+            elif (name == 'square'):
+                # square lattice
+                self.dimension = 2
+                self.bravais = 'sq'
+                self.lattice_constant = np.array([1.0, 1.0])
+                self.lattice_angle = np.array([90.0])
+                self.cell_bases = np.eye(2, order='F')
+                self.site_labels = ['S',]
+                self.sites = [[np.array([0.0, 0.0]), 0],]
+                self.coupling_constants = [1.0]
+                self.bonds = [[0, 0, np.array([1, 0], np.int32), 0],
+                              [0, 0, np.array([0, 1], np.int32), 0]]
+            else:
+                raise RuntimeError('unknown lattice name')
+        self.volume = abs(np.linalg.det(self.cell_bases))
+
+    def parse(self, path):
+        """
+        Read effective lattice model information from AkaiKKR output file
+
+        Parameters
+        ----------
+        path : path to the input file
+        """
+        self.dimension = 3
+        self.bravais = ''
+        self.lattice_constant = []
+        self.lattice_angle = []
+        self.cell_bases = np.zeros([3,3], order='F')
+        self.site_labels = []
+        self.sites = []
+        self.coupling_constants = []
+        self.bonds = []
+        meVinK = 1.16045221e+1 # meV in unit of Kelvin
+        section = ''
+        site_type_map = {}
+        with open(path) as f:
+            for line in f:
+                words = line.split()
+                if (words == ['lattice', 'constant']):
+                    section = 'lattice'
+                if (section == 'lattice'):
+                    if (len(words) > 3 and words[3] == 'c/a='):
+                        self.bravais = words[0].split('=')[1]
+                        a = float(words[2])
+                        self.lattice_constant = np.array([a, a * float(words[6]), a * float(words[4])])
+                    if (len(words) > 0 and words[0] == 'alpha='):
+                        self.lattice_angle = np.array([float(words[1]), float(words[3]), float(words[5])])
+
+                if (words[0:3] == ['primitive', 'translation', 'vectors']):
+                    section = 'primitive'
+                if (section == 'primitive'):
+                    if (len(words) > 0 and words[0] == 'a=('):
+                        self.cell_bases[0][0] = float(words[1])
+                        self.cell_bases[1][0] = float(words[2])
+                        self.cell_bases[2][0] = float(words[3].split(')')[0])
+                    if (len(words) > 0 and words[0] == 'b=('):
+                        self.cell_bases[0][1] = float(words[1])
+                        self.cell_bases[1][1] = float(words[2])
+                        self.cell_bases[2][1] = float(words[3].split(')')[0])
+                    if (len(words) > 0 and words[0] == 'c=('):
+                        self.cell_bases[0][2] = float(words[1])
+                        self.cell_bases[1][2] = float(words[2])
+                        self.cell_bases[2][2] = float(words[3].split(')')[0])
+
+                if (words == ['type', 'of', 'site']):
+                    section = 'type'
+                if (section == 'type'):
+                    if (len(words) > 1 and words[1] == 'rmt='):
+                        t = words[0].split('=')[1]
+                        i = len(self.site_labels)
+                        self.site_labels.append(t)
+                        site_type_map[t] = i
+
+                if (words == ['atoms', 'in', 'the', 'unit', 'cell']):
+                    section = 'atoms'
+                if (section == 'atoms'):
+                    if (len(words) > 0 and words[0] == 'position='):
+                        self.sites.append([np.array([float(words[1]), float(words[2]), float(words[3])]),
+                                           site_type_map[words[4].split('=')[1]]])
+
+                if (words == ['J_ij']):
+                    section = 'J_ij'
+                if (section == 'J_ij'):
+                    if (len(words) == 12):
+                        self.coupling_constants.append(2 * float(words[10]) * meVinK)
+
+                if (words[0:1] == ['pair#']):
+                    section = 'pair'
+                if (section == 'pair'):
+                    if (len(words) == 13):
+                        offset = np.array([int(words[6]), int(words[7]), int(words[8])], np.int32)
+                        if (self.__valid_offset(offset)):
+                            self.bonds.append([int(words[1])-1, int(words[2])-1, offset, int(words[12])-1])
+
+                if (len(words) == 0):
+                    if (section != 'J_ij'):
+                        section = ''
+                if (words[0:5] == ['Tc', '(in', 'mean', 'field', 'approximation)']):
+                    section = ''
+
+    def __valid_offset(self, offset):
+        """
+        For removing duplicated bond information (internal use)
+        """
+        if (offset[0] + offset[1] + offset[2] < 0):
+            return False
+        elif (offset[0] + offset[1] + offset[2] == 0):
+            if (offset[0] + offset[1] < 0):
+                return False
+            elif (offset[0] + offset[1] == 0):
+                if (offset[0] < 0):
+                    return False
+        return True
+
+if __name__ == '__main__':
+    import sys
+    path = ''
+    name = 'simple cubic' # 'simple cubic' or 'square'
+    if (len(sys.argv) > 2):
+        path = sys.argv[1]
+        name = sys.argv[2]
+    else:
+        print('Error: {} path name'.format(sys.argv[0]))
+        sys.exit(127)
+    p = unitcell(path, name)
+    print('[[input]]')
+    print('path: {}'.format(path))
+    print('name: {}'.format(name))
+    print('[[unitcell]]')
+    print('dimension: {}'.format(p.dimension))
+    print('lattice constants: {}'.format(p.lattice_constant))
+    print('unit cell basis vectors:')
+    for i in  range(p.dimension):
+        for j in  range(p.dimension):
+            sys.stdout.write('{} '.format(p.cell_bases[i][j]))
+        sys.stdout.write('\n')
+    print('unit cell volume: {}'.format(p.volume))
+    print('number of sites: {}'.format(len(p.sites)))
+    print('sites:', p.sites)
+    print('number of bonds:  {}'.format(len(p.bonds)))
+    print('bonds:', p.bonds)
+    print('site labels:', p.site_labels)
+    print('coupling constants:', p.coupling_constants)
+
+    lattices = ET.Element('LATTICES')
+
+    lattice = ET.SubElement(lattices, 'LATTICE')
+    lattice.set('name', name)
+    lattice.set('dimension', str(p.dimension))
+    basis = ET.SubElement(lattice, 'BASIS')
+    for j in range(p.dimension):
+        vector = ET.SubElement(basis, 'VECTOR')
+        el = []
+        for i in range(p.dimension):
+            el.append(str(p.cell_bases[i][j]))
+        vector.text = ' '.join(el)
+
+    unitcell = ET.SubElement(lattices, 'UNITCELL')
+    unitcell.set('name', name)
+    unitcell.set('dimension', str(p.dimension))
+    for s in p.sites:
+        vertex = ET.SubElement(unitcell, 'VERTEX')
+        vertex.set('type', str(s[1]))
+        coordinate = ET.SubElement(vertex, 'COORDINATE')
+        el = []
+        for i in range(p.dimension):
+            el.append(str(s[0][i]))
+        coordinate.text = ' '.join(el)
+    for b in p.bonds:
+        edge = ET.SubElement(unitcell, 'EDGE')
+        edge.set('type', str(b[1]))
+        source = ET.SubElement(edge, 'SOURCE')
+        source.set('vertex', str(b[0]))
+        target = ET.SubElement(edge, 'TARGET')
+        target.set('vertex', str(b[1]))
+        el = []
+        for i in range(p.dimension):
+            el.append(str(b[2][i]))
+        target.set('offset', ' '.join(el))
+
+    root = ET.ElementTree(lattices)
+    with open("lattice_" + name + ".xml", mode='w') as f:
+        f.write(ET.tostring(lattices, encoding="unicode"))
+
+    with open("coupling_" + name + ".dat", mode='w') as f:
+        for i in range(len(p.coupling_constants)):
+            f.write('{} {}'.format(i, p.coupling_constants[i]))
