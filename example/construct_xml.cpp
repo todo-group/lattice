27b7d43654f748a891e87b359871965d513a2c55
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-07-25 15:48:58 +0900

    Update construct_xml.cpp

--- example/construct_xml.cpp
+++ example/construct_xml.cpp
@@ -17,15 +17,43 @@
 #include <iostream>
 #include "lattice/graph_xml.hpp"
 
-int main() {
+int main(int argc, char **argv) {
   std::string file = "lattices.xml";
+  std::string basis_name = "square lattice";
+  std::string cell_name = "simple2d";
+  std::size_t length = 4;
+  if (argc > 1) {
+    if (argc == 4 || argc == 5) {
+      file = argv[1];
+      basis_name = argv[2];
+      cell_name = argv[3];
+      if (argc == 5) length = atoi(argv[4]);
+    } else {
+      std::cerr << "Error: " << argv[0] << " xmlfile basis cell [length]\n";
+      exit(127);
+    }
+  }
+
   std::ifstream is(file);
   boost::property_tree::ptree pt;
   read_xml(is, pt);
   lattice::basis bs;
-  read_xml(pt, "square lattice", bs);
+  read_xml(pt, basis_name, bs);
   lattice::unitcell cell;
-  read_xml(pt, "simple2d", cell);
-  lattice::graph lat(bs, cell, lattice::extent(4, 4));
-  lat.print(std::cout);
+  read_xml(pt, cell_name, cell);
+  switch (cell.dimension()) {
+  case 1:
+    { lattice::graph lat(bs, cell, lattice::extent(length)); lat.print(std::cout); }
+    break;
+  case 2:
+    { lattice::graph lat(bs, cell, lattice::extent(length, length)); lat.print(std::cout); }
+    break;
+  case 3:
+    { lattice::graph lat(bs, cell, lattice::extent(length, length, length)); lat.print(std::cout); }
+    break;
+  default:
+    std::cerr << "Unsupported lattice dimension\n";
+    exit(127);
+    break;
+  }
 }
