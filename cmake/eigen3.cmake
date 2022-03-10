bf3acaefc60a2de7eef227bcc357a3b3f8f6bf99
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-03-10 10:42:07 +0900

    update

--- cmake/eigen3.cmake
+++ cmake/eigen3.cmake
@@ -8,6 +8,6 @@ else(Eigen3_FOUND)
     URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2
     SOURCE_SUBDIR none
   )
-  include_directories(${eigen3_SOURCE_DIR})
+  list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/eigen3-src")
   list(APPEND FetchContents eigen3)
 endif(Eigen3_FOUND)
